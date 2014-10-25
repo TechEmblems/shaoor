6003ebbd57dcfec61219826d925c3935d6842fa3
RQ Bukhari <rq.bukhari@gmail.com>
2014-10-25 05:55:09 +0500

    Circular dependency bug fixed

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module Regdevice
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.paths.add File.join("app", "api"), glob: File.join("**", "*.rb")
-    config.autoload_paths += Dir[Rails.root.join("app", "api", "*")]
+    # config.autoload_paths += Dir[Rails.root.join("app", "api", "*")]
   end
 end
