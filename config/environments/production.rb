16bf595ddb517c1161a79c5e96af7c099a2d2c5e
Shahroon Ali <shahroon.ali@gmail.com>
2014-10-28 16:36:48 +0500

    rails admin assets issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.precompile += %w(rails_admin/rails_admin.css rails_admin/rails_admin.js)
 
   # Generate digests for assets URLs.
   config.assets.digest = true
