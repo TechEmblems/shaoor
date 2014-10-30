e18ddf48f9018efb48a0487f2fe2ae210783c56d
Shahroon Ali <shahroon.ali@gmail.com>
2014-10-30 19:51:51 +0500

    assets precompilation issue

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,10 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.compile = true
-Rails.application.config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
+#Rails.application.config.assets.precompile +=  %w(*.js *.css)
+Rails.application.config.assets.precompile +=  %w(registrations.js bootstrap.js classie.js device.js easing.js home.js jquery.jscrollpane.min.js
+jquery.magnific-popup.js jquery.min.js modernizr.custom.js move-top.js responsiveslides.min.js)
+Rails.application.config.assets.precompile +=  %w(registrations.css bootstrap_and_overrides.css component.css home.css
+popup-box.css simple-sidebar.css user-section.css)
 # Precompile additional assets.
 # application.js, application.css.scss, and all non-JS/CSS in app/assets folder are already added.
