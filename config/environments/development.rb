06868f4f74833ed541b725d2e70f579b67f50326
Shahroon <shahroon.ali@gmail.com>
2014-11-04 02:06:21 +0500

    mail setup with localhost settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,9 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => "1025"}
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'regdevice.com' }
-  config.action_mailer.smtp_settings = { :address => 'smtp.regdevice.com', :domain => 'regdevice.com' }
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => '25', :domain => 'regdevice.com' }
   config.action_mailer.logger = ActiveSupport::BufferedLogger.new( Rails.root.join('log', 'mailer.log') )
   config.action_mailer.logger.level = ActiveSupport::BufferedLogger::Severity::INFO
 end
