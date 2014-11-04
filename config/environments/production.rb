cbaefb93a26fc7e3e801a60f78d16ef175f47ac1
Shahroon <shahroon.ali@gmail.com>
2014-11-04 17:52:03 +0500

    testing devise mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.delivery_method = :sendmail
