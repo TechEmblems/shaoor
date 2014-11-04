03c47465f4a5199b92555f76b8b501887dae4ad8
Shahroon <shahroon.ali@gmail.com>
2014-11-04 16:52:53 +0500

    contact-us removed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources "contacts", only: [:new, :create]
 
   devise_for :users, :controllers => { :sessions => 'users/sessions', :registrations => 'users/registrations' }
 
