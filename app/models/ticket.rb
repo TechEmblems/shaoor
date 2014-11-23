64b3aa1b622b3a55c9cb1e590ade9cc183e58a68
Muneeb Meer <meer_online@yahoo.com>
2014-11-23 23:35:31 +0500

    after create ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,9 +3,12 @@ class Ticket < ActiveRecord::Base
   TYPE = %w[Sale Technical Development]
   STATUS = %W[Open Pending Closed Hold]
 
-
   belongs_to :user
   belongs_to :agent, class: 'User', foreign_key: :agent_id
   has_many :responses
+
+
+  #after_create :send_notification
+  #TODO send notifiction when the ticket is created
 
 end
